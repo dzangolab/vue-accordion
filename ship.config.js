a3f80488c4542f9983ac52cabfc1f1410623c0a5
Olivier Pichon <op@dzango.com>
2022-01-08 18:24:56 +0700

    chore: fix shipjs config

--- ship.config.js
+++ ship.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   publishCommand: ({ defaultCommand, tag }) =>
     `${defaultCommand} --access public --tag ${tag}`,
+  shouldPrepare: ({releaseType}) => {
+    if (releaseType === 'major' || releaseType === 'minor') {
+      return true;
+    }
+  },
 };
